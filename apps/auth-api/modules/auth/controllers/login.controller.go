d1c1d757b1dc03aec449e52fc19f54b13dc37455
LucaLucareli <luca.lucareli@germinare.org.br>
2026-01-15 16:29:19 -0300

    fix: improve error handling in login controller

--- apps/auth-api/modules/auth/controllers/login.controller.go
+++ apps/auth-api/modules/auth/controllers/login.controller.go
@@ -4,7 +4,9 @@ import (
 	"auth-api/modules/auth/dto/io"
 	"auth-api/modules/auth/dto/request"
 	"auth-api/modules/auth/services"
+	"net/http"
 	"shared/interfaces"
+	exceptionfactory "shared/validation/exception-factory"
 
 	"github.com/labstack/echo/v4"
 )
@@ -23,11 +25,14 @@ func (ctrl *LoginController) LoginController(ctx echo.Context) error {
 	var req request.LoginRequestDTO
 
 	if err := ctx.Bind(&req); err != nil {
-		return echo.NewHTTPError(400, "payload inválido")
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "JSON inválido"})
 	}
 
 	if err := ctx.Validate(&req); err != nil {
-		return echo.NewHTTPError(400, err.Error())
+		if httpErr := exceptionfactory.CustomExceptionFactory(err); httpErr != nil {
+			return ctx.JSON(httpErr.Code, httpErr.Message)
+		}
+		return ctx.JSON(http.StatusBadRequest, map[string]string{"message": "erro desconhecido"})
 	}
 
 	output, err := ctrl.loginService.LoginService(ctx.Request().Context(), io.LoginInputDTO{
